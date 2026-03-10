c75c004e68780dfcccd477997f777f100a617d9a
NguyenVuDangKhoa <25520876@gm.uit.edu.vn>
2026-03-10 13:52:49 +0700

    Add files via upload

--- HocSinh/main.cpp
+++ HocSinh/main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+ struct SV
+ {
+     string HoTen;
+     double toan;
+     double  van;
+     double DTB;
+ };
+ void Xuat(SV a)
+ {
+     cout<<fixed<<setprecision(2)<<a.DTB;
+ }
+ void NhapXuat(SV&a)
+ {
+     cin>>a.HoTen>>a.toan>>a.van;
+     a.DTB=(a.toan+a.van)/2;
+     Xuat(a);
 
+ }
+
+int main()
+{
+    SV a;
+    NhapXuat(a);
+    return 0;
+}
